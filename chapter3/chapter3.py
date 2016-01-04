fdddb660a5fc92f4db0106cdbf94e99fc7a0113a
Clark <song111@illinois.edu>
2016-01-05 00:10:40 +0800

    chapter3.py updated

--- chapter3/chapter3.py
+++ chapter3/chapter3.py
@@ -2,13 +2,15 @@
 import numpy as np
 import scipy as sp
 import os
+
 #bag of word
 from sklearn.feature_extraction.text import CountVectorizer
 
-vectorizer = CountVectorizer(min_df=1)
+#minimum count is 1, separate stop words
+vectorizer = CountVectorizer(min_df=1, stop_words='english')
 
 #find the most relavant post to "imaging databases"
-DIR = os.path.join("/Users/ClarkSong/Desktop/pythonML/chapter3")
+DIR = r"/Users/ClarkSong/Desktop/pythonML/chapter3/posts"
 posts = [open(os.path.join(DIR,f)).read() for f in os.listdir(DIR)]
 X_train = vectorizer.fit_transform(posts)
 
@@ -24,6 +26,13 @@ def dist_raw(v1,v2):
 	delta = v1-v2
 	return sp.linalg.norm(delta.toarray())
 
+#normalized distance
+def dist_norm(v1,v2):
+	v1_normalized = v1/sp.linalg.norm(v1.toarray())
+	v2_normalized = v2/sp.linalg.norm(v2.toarray())
+	delta = v1_normalized - v2.normalized 
+	return sp.linalg.norm(delta.toarray())
+
 
 #iterate all post and find the nearest
 best_doc = None
@@ -32,7 +41,7 @@ best_i = None
 for i in range(0, num_samples):
 	post= posts[i]
 	#skip the post itself
-	if post = new_post:
+	if post == new_post:
 		continue
 	post_vec = X_train.getrow(i)
 	cur_dist = dist_raw(new_post_vec,post_vec)
@@ -40,3 +49,13 @@ for i in range(0, num_samples):
 		best_dist = cur_dist
 		best_i = i
 		best_doc = posts[i]
+
+
+#Clustering K-Means
+vectorizer2 = StemmedTfidVectorizer(min_df=10,max_df=0.5,stop_words='english',charset_error='ignore')
+vectorized = vectorizer2.fit_transform(dataset.data)
+num_cluster = 50
+from sklearn.cluster import KMeans
+km = KMeans(n_cluster=num_cluster, init='random', n_init=1,verbose=1)
+km.fit(vectorized)
+
