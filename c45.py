673606c232228445c07e2ce661e779e0c36ba212
Kal-El-Git <kalel.github@gmail>
2016-01-26 20:00:13 -0500

    Update c45.py

--- c45.py
+++ c45.py
@@ -5,4 +5,25 @@ with Tree pruning and feature engineering
 C4.5
 
 '''
+def calcEntropy(dataSet):
+  numEntries = len(dataSet)
+  labelCounts ={}
+  for featVec in dataSet:
+    currentLabel = featVec[-1]
+    if currentLabel not in labelCounts:
+      labelCounts[currentLabel]=0
+    labelCounts[currentLabel]+=1
+  Ent = 0.0
+  for key in labelCounts:
+    prob = float(labelCounts[key])/numEntries
+    Ent-=prob*log(prob)
+  return Ent
 
+#Apply Info Gain Ratio
+def findBestSplit(dataSet):
+  numFeatures = len(dataSet)-1
+  baseEntropy = calcEntropy(dataSet)
+  bestRatio=0.0
+  bestFeat =-1
+  
+  return bestFeat
