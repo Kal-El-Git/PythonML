b155fa2cc28e5de56b3614d2f72562bfb1f7bd51
Kal-El-Git <ysong88@bu.edu>
2016-02-01 12:44:13 -0500

    Update c45.py

--- c45.py
+++ c45.py
@@ -4,7 +4,28 @@ with Tree pruning and feature engineering
 
 C4.5
 
+Pseudo Code
+
+generate d_tree
+create node N
+if all in same class:
+  return N
+if attr_list is empty:
+  return majority
+best feature = findBestFeat()
+mark N as best feature
+if best feature is discrete and multi-class:
+  remove from feature list
+for each class from split:
+  D is a subset
+  if D is empty:
+    create a node that is marked with best feature
+  else
+    recursively call d_tree, attach the node on N
+return N
 '''
+
+#Calculate the entropy, or, the Info of dataset
 def calcEntropy(dataSet):
   numEntries = len(dataSet)
   labelCounts ={}
@@ -19,11 +40,25 @@ def calcEntropy(dataSet):
     Ent-=prob*log(prob)
   return Ent
 
+
 #Apply Info Gain Ratio
 def findBestSplit(dataSet):
   numFeatures = len(dataSet)-1
   baseEntropy = calcEntropy(dataSet)
   bestRatio=0.0
   bestFeat =-1
+  for i in range(numFeatures):
+		featValues = [example[i] for example in dataSet]
+		uniqueFeatValues = set(featValues)
+		newEntropy = 0.0
+		for val in uniqueFeatValues:
+			subDataSet = splitData(dataSet,i,val)
+			prob = len(subDataSet)/float(len(dataSet))
+			newEntropy += prob*calcShannonEnt(subDataSet)
+		gain = baseEntropy - newEntropy	
+		if ( gain / newEntropy) > bestRatio:
+			bestRatio = gain / newEntropy
+			baseFeat = i
+	return bestFeat
   
   return bestFeat
