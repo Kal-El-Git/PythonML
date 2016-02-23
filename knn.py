4c3120bda20cbbf9b82f0dae5521fe3b934ed5c6
Kal-El-Git <ysong88@bu.edu>
2016-02-22 20:51:08 -0500

    Create knn.py

---
+++ knn.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def distance(p0,p1):
+	return np.sum((p0-p1)**2)
+
+def nn_classify(training_set, training_labels,new_example):
+	dists= np=np.array([distance(t,new_example) 
+		for t in training_set])
+	nearest = dists.argmin()
+	return training_labels[nearest]
+
+# normalize with Z distribution
+features -= features.mean(axis=0)
+features /= features.std(axis=0)
+
