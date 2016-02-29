d2d43da23664d899d4eba4c300e214a5940f7d04
Kal-El-Git <ysong88@bu.edu>
2016-02-29 17:07:01 -0500

    Update knn.py

--- knn.py
+++ knn.py
@@ -1,16 +1,55 @@
-# K Nearest Neighbor classifier
-import numpy as np
 
-def distance(p0,p1):
-	return np.sum((p0-p1)**2)
+# KNN  K-Nearest-Neighbor implementation in Python
 
-def nn_classify(training_set, training_labels,new_example):
-	dists= np=np.array([distance(t,new_example) 
-		for t in training_set])
-	nearest = dists.argmin()
-	return training_labels[nearest]
+'''
+Pros: high accuracy, insensitive to outliers, no data entry assumption
+Cons: high time complexity, high space complexity
+suitable for: numerical, category  
 
-# normalize with Z distribution
-features -= features.mean(axis=0)
-features /= features.std(axis=0)
+'''
 
+# usually k<=20
+'''
+Process Description
+1) collect data 
+2) data integration
+3) analyzed
+4) training
+5) testing
+6) use algorithm to classify
+'''
+
+# load data (dataset and labels)
+from numpy import *
+import operator
+def createDataSet():
+	group = array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
+	labels = ['A','A','B','B']
+	return group,labels
+
+'''
+Algorithm Description
+1) calculate distance between known points and test points
+2) sort in ASC order
+3) pick k points that have smallest distance
+4) calculate frequency of labels
+5) return the highest frequency as label
+
+'''
+
+def classfiy(inX, dataSet,labels,k):
+	#Calculate the distance 
+	dataSetSize = dataSet.shape[0]
+	diffMat = tile(inX, (dataSetSize,1))- dataSet
+	sqDiffMat = diffMat**2
+	sqDistances = sqDiffMat.sum(axis=1)
+	distances = sqDistances**0.5
+	sortedDistIndicies = distances.argsort()
+	#Count the labels and sort
+	classCount={}
+	for i in range(k):
+		voteIlabel = labels[sortedDistIndicies[i]] 
+		classCount[voteIlabel] = classCount.get(voteIlabel,0)+1
+	sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1),reverse=True)
+	#Sort the dictionary by key and return the key with largest vote
+	return sortedClassCount[0][0]
